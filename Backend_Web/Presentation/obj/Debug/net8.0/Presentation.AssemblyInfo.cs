13bd41727dd390a87d28c704c5d1adef1499c15f
AlaaZreekie <alaasama282@gmail.com>
2025-07-24 12:35:20 +0300

    BeginCourseGroub

--- Backend_Web/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Backend_Web/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3de98daebb0fbbbd15ac70c1592d09d4c9f72c6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf234526da97a4f70a143392d8ef500da7174e28")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
