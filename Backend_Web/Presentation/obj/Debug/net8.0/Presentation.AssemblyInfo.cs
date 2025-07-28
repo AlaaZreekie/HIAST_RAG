4a1e9ab7bb2d94af5c656ffafb567f436022abb3
AlaaZreekie <alaasama282@gmail.com>
2025-07-28 16:39:47 +0300

    ReAddTest

--- Backend_Web/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Backend_Web/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281a93d6322d94c64cebec1915642970fdf23259")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a187bc2e97f766905f5128664ef43ac7e5b1aea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
