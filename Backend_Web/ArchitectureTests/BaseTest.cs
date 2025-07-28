74705d3c73284fb8d140a4feac81251887681e53
AlaaZreekie <alaasama282@gmail.com>
2025-07-28 16:11:05 +0300

    AddUnitArchTesting

---
+++ Backend_Web/ArchitectureTests/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entity.IdentityEntity;
+using Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ArchitectureTests
+{
+    public abstract class BaseTest
+    {
+        protected static readonly Assembly DomainAssembly = typeof(ApplicationUser).Assembly;
+        protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+        protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+        protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+    }
+}
